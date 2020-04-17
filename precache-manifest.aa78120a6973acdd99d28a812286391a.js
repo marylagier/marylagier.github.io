a538d1acfb47c042330e4ad75f9fb5f0a3e25751
Mary Lagier <marylagier@gmail.com>
2020-04-17 14:34:25 -0700

    Updates

---
+++ precache-manifest.aa78120a6973acdd99d28a812286391a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "05e564f2819153ab855c53879d24b49f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a8bc14685fb9e96adfe0",
+    "url": "/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "d9e0958b4ad163542746",
+    "url": "/static/css/main.0894dde9.chunk.css"
+  },
+  {
+    "revision": "a8bc14685fb9e96adfe0",
+    "url": "/static/js/2.8a130a36.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.8a130a36.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d9e0958b4ad163542746",
+    "url": "/static/js/main.cb7e6729.chunk.js"
+  },
+  {
+    "revision": "9467a65409a016d58f0a",
+    "url": "/static/js/runtime-main.39b3bc91.js"
+  }
+]);
